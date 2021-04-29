199e03c7ef1c3002d6ee1385316693c7e9d02424
Plouss <80962741+Plouss@users.noreply.github.com>
2021-04-29 18:40:52 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let monTab = ['pomme', 'fraise', 'melon', 'citron'];
+monTab.pop();
+console.log()
+monTab.shift();
+console.log(monTab);
+monTab.push("pasteque");
+console.log(monTab);
+monTab.unshift("orange");
+monTab[1] = monTab[1].toUpperCase();
+console.log(monTab);
+monTab.reverse
+console.log();
+
+let chiffres = [4,5,2,1,3]
